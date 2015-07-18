780d1e55bbba557a271f69a93c12827f22226a77
vinicius-sabadim <vinicius.sabadim@gmail.com>
2015-07-18 13:19:35 -0300

    Testando se uma rota não existente cai no caso do otherwise

--- test/e2e/cookbookSpec.js
+++ test/e2e/cookbookSpec.js
@@ -27,9 +27,14 @@ describe('Roteamento', function() {
 		expect(button.isPresent()).toBeTruthy();
 	});
 
-	it('should navigate to emcees page on click of the load button', function () {
+	it('should navigate to emcees page on click of the load button', function() {
 		var button = element(by.buttonText('load'));
 		button.click();
 		expect(browser.getLocationAbsUrl()).toContain('/emcees');
+	});
+
+	it('should redirect to home page if an unknown url is provided', function() {
+		browser.get('http://0.0.0.0:8000/#/dummy');
+		expect(browser.getLocationAbsUrl()).toContain('/home');
 	});
 });
