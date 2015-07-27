fc120530afb9eb07b35d44d0876edb2fa5f196de
vinicius-sabadim <vinicius.sabadim@gmail.com>
2015-07-27 13:31:36 -0300

    Testando http requests usando spies

--- test/unit/service.js
+++ test/unit/service.js
@@ -1,20 +1,25 @@
 describe('service emcees', function() {
-	var emcees;
 
-	beforeEach(module('cookbook'));
+	describe('api', function() {
+		var emcees;
 
-	beforeEach(inject(function($injector) {
-		emcees = $injector.get('emcees');
-	}));
+		beforeEach(module('cookbook'));
 
-	it('should have a method defined', function() {
-		expect(emcees.getUKEmcees).toBeDefined();
+		beforeEach(inject(function($injector) {
+			emcees = $injector.get('emcees');
+		}));
+
+		it('should have a method defined', function() {
+			expect(emcees.getUKEmcees).toBeDefined();
+		});
 	});
 
 	describe('http get method', function() {
 		var emcees;
 		var $httpBackend;
 		var url;
+
+		beforeEach(module('cookbook'));
 
 		beforeEach(inject(function($injector) {
 			emcees = $injector.get('emcees');
@@ -38,10 +43,32 @@ describe('service emcees', function() {
 		});
 	});
 
+	describe('http get using spies', function() {
+		var emcees;
+		var httpMock;
+
+		beforeEach(module('cookbook', function($provide) {
+			httpMock = jasmine.createSpyObj('$http', ['get']);
+			$provide.value('$http', httpMock);
+		}));
+
+		beforeEach(inject(function($injector) {
+			emcees = $injector.get('emcees');
+		}));
+
+		it('should make a GET request', function() {
+			emcees.getUKEmcees();
+			expect(httpMock.get).toHaveBeenCalled();
+			expect(httpMock.get).toHaveBeenCalledWith('/emcees/uk');
+		});
+	});
+
 	describe('http post method', function() {
 		var emcees;
 		var $httpBackend;
 		var url;
+
+		beforeEach(module('cookbook'));
 
 		beforeEach(inject(function($injector) {
 			emcees = $injector.get('emcees');
@@ -61,6 +88,38 @@ describe('service emcees', function() {
 			$httpBackend.expectPOST(url, emcee).respond(201, '');
 			emcees.addUKEmcee(emcee);
 			$httpBackend.flush();
+		});
+	});
+
+	describe('http post using spies', function() {
+		var emcees;
+		var httpMock;
+		var url;
+
+		beforeEach(module('cookbook', function($provide) {
+			httpMock = jasmine.createSpyObj('$http', ['post']);
+			$provide.value('$http', httpMock);
+		}));
+
+		beforeEach(inject(function($injector) {
+			emcees = $injector.get('emcees');
+			url = '/emcees/uk';
+		}));
+
+		it('should make a POST request', function() {
+			var emcee = {
+				'name': 'alkaline'
+			};
+			emcees.addUKEmcee(emcee);
+			expect(httpMock.post).toHaveBeenCalled();
+		});
+
+		it('should make a POST request with correct data', function() {
+			var emcee = {
+				'name': 'tlp'
+			};
+			emcees.addUKEmcee(emcee);
+			expect(httpMock.post).toHaveBeenCalledWith(url, emcee);
 		});
 	});
 
